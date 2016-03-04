89a036ef4a064a1a58c93e20af6311878fd76838
Tiplok <tiplok1@gmail.com>
2016-03-04 14:21:19 +0100

    DB update
    Mise à jour de la base de données.

--- bdd/pji.sql
+++ bdd/pji.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Ven 05 Février 2016 à 15:54
+-- Généré le :  Ven 04 Mars 2016 à 14:20
 -- Version du serveur :  5.6.17
 -- Version de PHP :  5.5.12
 
@@ -30,10 +30,20 @@ CREATE TABLE IF NOT EXISTS `user` (
   `PK_user` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(20) NOT NULL,
   `password` varchar(20) NOT NULL,
+  `actual_score` int(11) NOT NULL DEFAULT '0',
+  `best_score` int(11) NOT NULL DEFAULT '0',
   `date_register` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `date_last_login` datetime DEFAULT NULL,
   `auth_level` varchar(5) NOT NULL DEFAULT 'ok',
   PRIMARY KEY (`PK_user`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Contenu de la table `user`
+--
+
+INSERT INTO `user` (`PK_user`, `username`, `password`, `actual_score`, `best_score`, `date_register`, `date_last_login`, `auth_level`) VALUES
+(1, 'aaa', '$6lg.gqkOafmo', 0, 0, '2016-02-28 14:47:16', '2016-03-04 14:19:33', 'ok');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
