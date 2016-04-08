c22c26eeb5d1395bbcdf95a728ad31683dda6c19
Ramecourt Valentin <valentin.ramecourt@etudiant.univ-lille1.fr>
2016-04-08 17:27:49 +0200

    upgrade fonctions.js

--- utilities/fonctions.js
+++ utilities/fonctions.js
@@ -111,30 +111,33 @@ function processTooltip(){
     });
 }
 
-// En réalité, le nombre de lignes est de 9 et le nombre de colonnes est de 16, il faut changer et le schéma se retrouve tourner à 90° vers la droite (sens horaire)
-var nbRow = 16;
-var nbCol = 9;
+function createGrid(){
+    // En réalité, le nombre de lignes est de 9 et le nombre de colonnes est de 16, il faut changer et le schéma se retrouve tourner à 90° vers la droite (sens horaire)
+    var nbRow = 9;
+    var nbCol = 16;
 
-//var case = new Object();
+    //var case = new Object();
 
-var grid = [
-    ["E", "E", "E", "E", "E", "E", "E", "E", "E"], // L1
-    ["R", "R", "E", "E", "E", "E", "E", "E", "E"], // L2
-    ["E", "R", "R", "R", "R", "T", "E", "T", "T"], // L3
-    ["E", "T", "T", "E", "R", "T", "E", "E", "E"], // L4
-    ["E", "T", "T", "E", "R", "E", "E", "E", "E"], // L5
-    ["E", "E", "E", "E", "R", "E", "E", "E", "E"], // L6
-    ["E", "E", "E", "E", "R", "E", "E", "E", "E"], // L7
-    ["T", "E", "E", "E", "R", "T", "E", "E", "E"], // L8
-    ["E", "E", "R", "R", "R", "E", "E", "T", "T"], // L9
-    ["E", "E", "R", "E", "E", "E", "E", "R", "R"], // L10
-    ["E", "T", "R", "E", "E", "E", "E", "R", "E"], // L11
-    ["E", "T", "R", "E", "E", "T", "T", "R", "E"], // L12
-    ["E", "E", "R", "E", "E", "T", "T", "R", "E"], // L13
-    ["E", "E", "R", "R", "R", "R", "R", "R", "T"], // L14
-    ["E", "E", "E", "E", "E", "E", "E", "E", "E"], // L15
-    ["T", "E", "E", "E", "E", "E", "E", "E", "E"]  // L16
-]
+    var grid = [
+        ["E", "E", "E", "E", "E", "E", "E", "E", "E"], // L1
+        ["R", "R", "E", "E", "E", "E", "E", "E", "E"], // L2
+        ["E", "R", "R", "R", "R", "T", "E", "T", "T"], // L3
+        ["E", "T", "T", "E", "R", "T", "E", "E", "E"], // L4
+        ["E", "T", "T", "E", "R", "E", "E", "E", "E"], // L5
+        ["E", "E", "E", "E", "R", "E", "E", "E", "E"], // L6
+        ["E", "E", "E", "E", "R", "E", "E", "E", "E"], // L7
+        ["T", "E", "E", "E", "R", "T", "E", "E", "E"], // L8
+        ["E", "E", "R", "R", "R", "E", "E", "T", "T"], // L9
+        ["E", "E", "R", "E", "E", "E", "E", "R", "R"], // L10
+        ["E", "T", "R", "E", "E", "E", "E", "R", "E"], // L11
+        ["E", "T", "R", "E", "E", "T", "T", "R", "E"], // L12
+        ["E", "E", "R", "E", "E", "T", "T", "R", "E"], // L13
+        ["E", "E", "R", "R", "R", "R", "R", "R", "T"], // L14
+        ["E", "E", "E", "E", "E", "E", "E", "E", "E"], // L15
+        ["T", "E", "E", "E", "E", "E", "E", "E", "E"]  // L16
+    ]
+}
+
 
 var tree_types = {
     chene : {oxygen:20, cost:9000, water_need: 200},
